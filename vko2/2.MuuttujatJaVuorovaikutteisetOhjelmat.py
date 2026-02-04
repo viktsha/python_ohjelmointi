8fc564bff3a1a9d99adced8cc1e6153c2d51376e
shaliupinavictoria <shalupina.victoriia@icloud.com>
2026-02-04 10:11:39 +0200

    Initial commit

---
+++ vko2/2.MuuttujatJaVuorovaikutteisetOhjelmat.py
@@ -0,0 +1,17 @@
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+
+import math
+radius = float(input("Enter your radius: "))
+area = math.pi * radius ** 2
+print(math.pi * radius ** 2)
+
+import math
+base = float(input("Enter the base of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+perimeter = 2 * (base + height)
+area = base * height
+print("The perimeter of the rectangle is:", perimeter)
+print("The area of the rectangle is:", area)
+
+
